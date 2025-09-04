d1bce2d0c7f647ff6f3f3e01df345c4e90efee19
Keerthana Murugesan <vmskeerthu@gmail.com>
2025-09-04 12:21:40 +0530

    Initial scaffold for banking microservices

---
+++ user-account-service/user-account-service/src/main/java/com/banking/user_account_service/UserAccountServiceApplication.java
@@ -0,0 +1,13 @@
+package com.banking.user_account_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAccountServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserAccountServiceApplication.class, args);
+	}
+
+}
---
+++ user-account-service/user-account-service/src/test/java/com/banking/user_account_service/UserAccountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banking.user_account_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAccountServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
